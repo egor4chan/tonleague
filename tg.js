98cabe5e0a61e1b962c5841d2b3e58875cfa230b
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-07-28 23:31:56 +0400

    Add files via upload

---
+++ tg.js
@@ -0,0 +1,4 @@
+var WebApp = window.Telegram.WebApp;
+
+WebApp.expand();
+WebApp.showAlert(WebApp.initDataUnsafe.user.username);
--- th.js
+++
@@ -1,4 +0,0 @@
-var WebApp = window.Telegram.WebApp;
-
-WebApp.expand();
-WebApp.showAlert(WebApp.initDataUnsafe.user.username);
