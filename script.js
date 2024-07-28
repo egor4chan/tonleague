97912e4b126b3f3ff8869937f544f7b5db895367
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-07-28 18:31:01 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+$('#ton').on('click', function() {
+    let energy = $('#myenergy').html();
+    let htmlEnergy= document.getElementById('myenergy')
+    let newEnergy = Number(energy) - 1;
+    let balance = $('#balance').html();
+    let htmlBalance = document.getElementById('balance')
+    let newBalance = parseFloat(balance) + 0.000001;
+
+    if (Number(energy) != 0) {
+        htmlEnergy.innerHTML = newEnergy;
+        htmlBalance.innerHTML = newBalance.toFixed(6);    
+    }
+    else {
+        console.log('Energy limit!')
+    }
+    
+})
+
+$('#mine').on('click', function() {
+
+})
+
+$('#shop').on('click', function() {
+
+})
+
+$('#profile').on('click', function() {
+    Location.href('/team.html')
+})
