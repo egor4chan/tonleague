ffafcc6966efb634f3c2f23754d7cad399f41bf1
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-07-29 19:25:42 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function shake() {
 }
 
 function click() {
+    
     shake();
     let energy = document.getElementById('myenergy').innerHTML; // $('#myenergy').html();
     let htmlEnergy= document.getElementById('myenergy')
@@ -21,11 +22,14 @@ function click() {
 
     if (Number(energy) != 0) {
         htmlEnergy.innerHTML = newEnergy;
-        htmlBalance.innerHTML = newBalance.toFixed(6);    
+        htmlBalance.innerHTML = newBalance.toFixed(6);
+        
     }
     else {
         console.log('Energy limit!')
     }
+
+    
 }
 
 function energy_limit() {
@@ -45,7 +49,7 @@ setInterval(energy_limit, 2000)
 var clicker = document.getElementById('clicker');
 clicker.onclick = click;
 var nick = document.getElementById('nick');
-nick.innerHTML = '@' + window.localStorage.getItem('nick');
+
 
 function add_clicker_effect(x, y) {
     let el = document.createElement("h6");
@@ -66,7 +70,8 @@ clicker.addEventListener('mousemove', function (event) {
     const x = event.clientX; // получаем координату X мыши
     const y = event.clientY; // получаем координату Y мыши
   
+    
     add_clicker_effect(x, y)
     
     console.log(`Координаты мыши: x=${x}, y=${y}`); // выводим координаты мыши в консоль
-  });
+})
